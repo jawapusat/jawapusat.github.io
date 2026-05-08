1905de61236d2c1c8c28c5bf656a63733dfa1bd0
jawapusat <adminjawapusat@gmail.com>
2026-05-08 12:18:38 +0700

    Initial commit - upload website

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-251.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-d1f2f60b', {"layout":"number","heading":"Tahun Pengalaman","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":15,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-6c75e0ad', {"layout":"number","heading":"Anggota Tim","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":20,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-dd716b88', {"layout":"number","heading":"Pelanggan Puas","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":98,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-d9cb5f03', {"layout":"number","heading":"Mendapatkan Penghargaan","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":5,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
